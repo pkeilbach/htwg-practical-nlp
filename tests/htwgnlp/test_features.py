63ee3869a049aa73c267e59e65187e33e7f14b4c
Pascal Keilbach <28002199+pkeilbach@users.noreply.github.com>
2023-11-20 06:41:39 +0100

    Logistic regression lecture (#36)
    * add matplotlib as project requirement
    
    * logistic regression lecture
    
    * code for assignment 2
    
    * add make command for assignment 2
    
    * add notebook for assignment 2

---
+++ tests/htwgnlp/test_features.py
@@ -0,0 +1,50 @@
+from collections import defaultdict
+
+import pytest
+import numpy as np
+
+from htwgnlp.features import CountVectorizer
+
+
+tweets = [
+    ["this", "is", "a", "tweet"],  # 1
+    ["a", "happy", "tweet"],  # 1
+    ["what", "a", "sad", "tweet"],  # 0
+]
+labels = np.array([[1], [1], [0], [1], [0]])
+
+
+@pytest.fixture
+def vectorizer():
+    vectorizer = CountVectorizer()
+    vectorizer.build_word_frequencies(tweets, labels)
+    return vectorizer
+
+
+def test_init():
+    vec = CountVectorizer()
+    assert isinstance(vec.word_frequencies, defaultdict)
+    assert vec.word_frequencies["some_non_existent_key"] == 0
+
+
+def test_build_word_frequencies(vectorizer):
+    assert vectorizer.word_frequencies[("this", 1)] == 1
+    assert vectorizer.word_frequencies[("is", 1)] == 1
+    assert vectorizer.word_frequencies[("a", 1)] == 2
+    assert vectorizer.word_frequencies[("tweet", 1)] == 2
+    assert vectorizer.word_frequencies[("what", 0)] == 1
+    assert vectorizer.word_frequencies[("a", 0)] == 1
+    assert vectorizer.word_frequencies[("sad", 0)] == 1
+    assert vectorizer.word_frequencies[("tweet", 0)] == 1
+
+
+@pytest.mark.parametrize(
+    "tweet, expected",
+    [
+        (["this", "is", "a", "test", "tweet"], np.array([1, 6, 2])),
+        (["some", "random", "tweet"], np.array([1, 2, 1])),
+        (["this", "is", "a", "sunny", "day"], np.array([1, 4, 1])),
+    ],
+)
+def test_get_features(vectorizer, tweet, expected):
+    assert np.array_equal(vectorizer.get_features(tweet), expected)
