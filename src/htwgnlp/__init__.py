c131d166bb9659c3e932c83cc91cfd66e69a12d4
AlexJauchWalser <alexander.jauch-walser@knime.com>
2024-11-12 14:12:07 +0100

    Add docstring describing the package content

--- src/htwgnlp/__init__.py
+++ src/htwgnlp/__init__.py
@@ -1,1 +1,31 @@
-""""This is an empty __init__.py file to make Python treat the directory as a package."""
+"""HTWG NLP Course Package
+
+This is an empty __init__.py file to make Python treat the directory as a package.
+
+The `htwgnlp` package contains code for the HTWG NLP lecture and assignments. To locate all
+code parts to be implemented for an assignments, search for the corresponding assignment
+number (0-4) using `ASSIGNMENT-X` as a search term.
+Test the code by running `make assignment-x` command from the root directory of the project.
+
+Assignments:
+
+    Assignment 0: Introduction to Python
+        A basic introduction to Python, covering programming concepts and language features.
+        File: python_basics.py
+
+    Assignment 1: Preprocessing
+        Introduction to text preprocessing, covering tokenization, stemming, and stopword removal.
+        File: preprocessing.py
+
+    Assignment 2: Text Classification
+        A basic introduction to classification, covering logistic regression and feature extraction.
+        Files: features.py, logistic_regression.py
+
+    Assignment 3: Naive Bayes
+        Experience with key concepts such as word frequency calculation, Laplace smoothing, log likelihoods, and predictive probabilities.
+        File: naive_bayes.py
+
+    Assignment 4: Text Embedding
+        Work with word embeddings for NLP tasks, focusing on loading, managing, and using vectorized word representations.
+        File: embeddings.py
+"""
